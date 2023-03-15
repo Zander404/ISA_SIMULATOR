3f4b09241e526dab10b6308d13cb84b9e96b0902
zander404 <alexandrearthurpg@gmail.com>
2023-03-15 15:04:22 -0300

    crinado projeto

---
+++ cpu.h
@@ -0,0 +1,53 @@
+//Formato de Instrucao 1
+#define hlt 0b00000
+#define nop 0b00001
+
+#define add 0b00010
+#define sub 0b00011
+#define mul 0b00100
+#define div 0b00101
+
+#define cmp 0b00110
+#define xchg 0b00111
+
+#define and 0b01000
+#define or 0b01001
+#define xor 0b01010
+#define not 0b01011
+
+//Formato de instrucao 2
+#define je 0b01100
+#define jne 0b01101
+
+#define jl 0b01110
+#define jle 0b01111
+
+#define jg 0b10000
+#define jge 0b10001
+
+#define jmp 0b10010
+
+#define lda 0b10011
+#define ldb 0b10100
+
+#define sta 0b10101
+#define stb 0b10110
+
+
+
+//Formato de Instrucao 1
+#define ldrb 0b10111
+
+
+
+//Formato de Instrucao 3
+#define movial 0b11000
+#define moviah 0b11001
+
+#define addia 0b11010
+#define subia 0b11011
+#define mulia 0b11100
+#define divia 0b11101
+
+#define lsh 0b11110
+#define rsh 0b11111
---
+++ main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "cpu.h"
+int main() {
+    printf("Hello, World! %i \n", hlt);
+    printf("hlt: %i\n",hlt);
+    printf("nop: %i\n",nop);
+    printf("add: %i\n",add);
+    printf("sub: %i\n",sub);
+    printf("mul: %i\n",mul);
+    printf("div: %i\n",div);
+
+    printf("cmp: %i\n",cmp);
+    printf("xchg: %i\n",xchg);
+    printf("and: %i\n",and);
+    printf("or: %i\n",or);
+    printf("xor: %i\n",xor);
+    printf("not: %i\n",not);
+
+    printf("je: %i\n",je);
+    printf("jne: %i\n",jne);
+    printf("jl: %i\n",jl);
+    printf("jle: %i\n",jle);
+    printf("jg: %i\n",jg);
+    printf("jge: %i\n",jge);
+
+    printf("jmp: %i\n",jmp);
+    printf("lda: %i\n",lda);
+    printf("ldb: %i\n",ldb);
+    printf("sta: %i\n",sta);
+    printf("stb: %i\n",stb);
+    printf("ldrb: %i\n",ldrb);
+
+    printf("movial: %i\n",movial);
+    printf("moviah: %i\n",moviah);
+
+    printf("addia: %i\n",addia);
+    printf("subia: %i\n",subia);
+    printf("mulia: %i\n",mulia);
+    printf("divia: %i\n",divia);
+
+    printf("lsh: %i\n",lsh);
+    printf("rsh: %i\n",rsh);
+
+
+    return 0;
+}
