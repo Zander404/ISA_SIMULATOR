bbce6f0d15018c8b21327e401482d8435f0c8c0e
xandy <alexandrearthurpg@gmail.com>
2023-04-02 21:28:33 -0300

    Changes by Pedrofff

--- cpu.h
+++ cpu.h
@@ -67,7 +67,7 @@
 #define maskA 0xffff0000         //| 1111 1111 | 1111 1111 | xxxx xxxx | xxxx xxxx
 #define maskB 0x0000ffff         //| xxxx xxxx | xxxx xxxx | 1111 1111 | 1111 1111
 
-#define maskstA 0x7ff           //| xxxx x111 | 1111 1111 |
+#define maskstA 0x7ff            //| xxxx x111 | 1111 1111 |
 #define lixo 0x8e                //| xxxx xxxx | 1111 1111 |
 #define lixo2 0x                 //| xxxx x111 | 1111
 //curl 10.42.0.137 -o "nome do arquivo"
